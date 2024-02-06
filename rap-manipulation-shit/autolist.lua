7d4be817f7e00d5b8489459425fad16b84187997
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-06 20:34:17 +0530

    Update autolist.lua
    changed to 1 huge at a time BRUH ;(

--- rap-manipulation-shit/autolist.lua
+++ rap-manipulation-shit/autolist.lua
@@ -4,14 +4,12 @@ local args1 = {
     [2] = 49806538,
     [3] = 1
 }
-local args2 = {
-    [1] = "43986e240e4f4c65bec80a1425240df6",
-    [2] = 49806538,
-    [3] = 1
-}
+-- local args2 = {
+--     [1] = "43986e240e4f4c65bec80a1425240df6",
+--     [2] = 49806538,
+--     [3] = 1
+-- }
 for i = 1, 100000, 1 do
     task.wait(5)
     ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
-    task.wait(5)
-    ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args2))
 end
