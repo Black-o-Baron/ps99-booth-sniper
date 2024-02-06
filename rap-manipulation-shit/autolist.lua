b242f34447cb8d4468ade6458d6ae3ed43465fcd
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-06 22:47:53 +0530

    Update autolist.lua
    changed price

--- rap-manipulation-shit/autolist.lua
+++ rap-manipulation-shit/autolist.lua
@@ -1,14 +1,9 @@
 local ReplicatedStorage1 = game:GetService("ReplicatedStorage")
 local args1 = {
     [1] = "6cdbbf11d30d4273b1f84ddd804b4392",
-    [2] = 49806538,
+    [2] = 99870654,
     [3] = 1
 }
--- local args2 = {
---     [1] = "43986e240e4f4c65bec80a1425240df6",
---     [2] = 49806538,
---     [3] = 1
--- }
 for i = 1, 100000, 1 do
     task.wait(5)
     ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
