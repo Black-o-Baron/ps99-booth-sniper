e66c85510a39d80319fa4819ba4758c380729587
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-06 11:14:17 +0530

    Create boothsniper.lua
    updated for huge ducky

---
+++ rap-manipulation-shit/boothsniper.lua
@@ -0,0 +1,62 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+task.wait(15)
+game.Players.LocalPlayer.PlayerScripts.Scripts.Core["Idle Tracking"].Enabled = false
+local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
+local Players = game:GetService('Players')
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local signal
+
+local vu = game:GetService("VirtualUser")
+Players.LocalPlayer.Idled:connect(function()
+    vu:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    vu:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+end)
+
+local function tryPurchase(uid, playerid, buytimestamp)
+    signal = game:GetService("RunService").Heartbeat:Connect(function()
+        if buytimestamp < workspace:GetServerTimeNow() then
+            signal:Disconnect()
+            signal = nil
+        end
+    end)
+    repeat task.wait() until signal == nil
+    ReplicatedStorage.Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+end
+
+Booths_Broadcast.OnClientEvent:Connect(function(username, message)
+    if type(message) == "table" then
+        local highestTimestamp = -math.huge -- Initialize with the smallest possible number
+        local key = nil
+        local listing = nil
+        for v, value in pairs(message["Listings"] or {}) do
+            if type(value) == "table" and value["ItemData"] and value["ItemData"]["data"] then
+                local timestamp = value["Timestamp"]
+                if timestamp > highestTimestamp then
+                    highestTimestamp = timestamp
+                    key = v
+                    listing = value
+                end
+            end
+        end
+        if listing then
+            local buytimestamp = listing["ReadyTimestamp"]
+            local data = listing["ItemData"]["data"]
+            local gems = tonumber(listing["DiamondCost"])
+            local uid = key
+            local item = data["id"]
+            local amount = tonumber(data["_am"]) or 1
+            local playerid = message['PlayerID']
+            local unitGems = gems / amount
+
+            print("ITEM NAME: " .. item)
+
+            if string.find(item, "Huge Ducky") and unitGems == 136870654 then
+                coroutine.wrap(tryPurchase)(uid, playerid, buytimestamp)
+            end
+        end
+    end
+end)
