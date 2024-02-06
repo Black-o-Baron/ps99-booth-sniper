c5a750a15a1f24d71cda8c3102624a6be8293064
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-06 19:53:44 +0530

    Update autolist.lua
    updated for huge redstone cat inflation

--- rap-manipulation-shit/autolist.lua
+++ rap-manipulation-shit/autolist.lua
@@ -1,10 +1,17 @@
 local ReplicatedStorage1 = game:GetService("ReplicatedStorage")
 local args1 = {
-    [1] = "54e0db755d7c4219bd464f55407ea36a",
-    [2] = 136870654,
+    [1] = "6cdbbf11d30d4273b1f84ddd804b4392",
+    [2] = 49806538,
+    [3] = 1
+}
+local args2 = {
+    [1] = "43986e240e4f4c65bec80a1425240df6",
+    [2] = 48867008,
     [3] = 1
 }
 for i = 1, 100000, 1 do
     task.wait(5)
     ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
+    task.wait(5)
+    ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args2))
 end
