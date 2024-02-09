37030fb91928af8ba123ef7c478aa68feb422ffc
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-09 18:03:50 +0530

    Update boothsniper.lua
    updated gem value

--- rap-manipulation-shit/boothsniper.lua
+++ rap-manipulation-shit/boothsniper.lua
@@ -54,7 +54,7 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
 
             print("ITEM NAME: " .. item)
 
-            if string.find(item, "Huge Festive Cat") and unitGems == 120765468 then
+            if string.find(item, "Huge Festive Cat") and unitGems == 60876654 then
                 coroutine.wrap(tryPurchase)(uid, playerid, buytimestamp)
             end
         end
