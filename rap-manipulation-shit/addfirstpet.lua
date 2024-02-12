9425740633afc3c8980221058981e8753a3805fc
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-12 19:00:42 +0530

    Create addfirstpet.lua

---
+++ rap-manipulation-shit/addfirstpet.lua
@@ -0,0 +1,6 @@
+local args = {
+    [1] = "cb8eda4d6593498e87e9901a5af48163",
+    [2] = 79867890,
+    [3] = 1
+}
+game:GetService("ReplicatedStorage").Network.Booths_CreateListing:InvokeServer(unpack(args))
