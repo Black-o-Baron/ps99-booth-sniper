8f28d978e7b473e05644f16cb630b2353e28db83
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-12 12:53:34 +0530

    Update autolist.lua
    added multiple huges

--- rap-manipulation-shit/autolist.lua
+++ rap-manipulation-shit/autolist.lua
@@ -1,10 +1,52 @@
 local ReplicatedStorage1 = game:GetService("ReplicatedStorage")
-local args1 = {
-    [1] = "cb8eda4d6593498e87e9901a5af48163",
-    [2] = 117890564,
-    [3] = 1
-}
-for i = 1, 100000, 1 do
-    task.wait(5)
+local args1
+for i = 1, 450, 1 do
+    args1 = {
+        [1] = "cb8eda4d6593498e87e9901a5af48163",
+        [2] = 46678800,
+        [3] = 1
+    }
+    task.wait(4)
     ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
+    task.wait(1)
+    args1 = {
+        [1] = "e23961d8fb554bfdb6a4419352994eea",
+        [2] = 46678800,
+        [3] = 1
+    }
+    task.wait(4)
+    ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
+    task.wait(1)
+    args1 = {
+        [1] = "b68abde8f91f4c789f6dabd543dc9500",
+        [2] = 46678800,
+        [3] = 1
+    }
+    task.wait(4)
+    ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
+    task.wait(1)
+    args1 = {
+        [1] = "ef4239db53904f0b9ff211c3dd330558",
+        [2] = 46678800,
+        [3] = 1
+    }
+    task.wait(4)
+    ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
+    task.wait(1)
+    args1 = {
+        [1] = "a55fa58c76e74c76a8dfee3f49378e56",
+        [2] = 46678800,
+        [3] = 1
+    }
+    task.wait(4)
+    ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
+    task.wait(1)
+    args1 = {
+        [1] = "a1af1113d2db440c9eddaf8a85daa6c6",
+        [2] = 46678800,
+        [3] = 1
+    }
+    task.wait(4)
+    ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
+    task.wait(1)
 end
