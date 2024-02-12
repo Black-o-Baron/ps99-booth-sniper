76ff902c18e97c556cff4b2e3f79cf16b2f50e09
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-12 13:45:43 +0530

    Update autolist.lua

--- rap-manipulation-shit/autolist.lua
+++ rap-manipulation-shit/autolist.lua
@@ -3,19 +3,19 @@ local args1
 for i = 1, 200, 1 do
     task.wait(11) -- 3 seconds for the listing to be published, 1.5 second to purchase -> for 2 accounts -> 9 seconds approx
     args1 = {
-        [1] = "cb8eda4d6593498e87e9901a5af48163",
+        [1] = "cb8eda4d6593498e87e9901a5af48163", -- axolotl
         [2] = 46678800,
         [3] = 1
     }
     ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
     -- args1 = {
-    --     [1] = "e23961d8fb554bfdb6a4419352994eea",
+    --     [1] = "e23961d8fb554bfdb6a4419352994eea", -- skeleton
     --     [2] = 46678800,
     --     [3] = 1
     -- }
     -- ReplicatedStorage1.Network.Booths_CreateListing:InvokeServer(unpack(args1))
     -- args1 = {
-    --     [1] = "ef4239db53904f0b9ff211c3dd330558",
+    --     [1] = "ef4239db53904f0b9ff211c3dd330558", -- mascot
     --     [2] = 46678800,
     --     [3] = 1
     -- }
