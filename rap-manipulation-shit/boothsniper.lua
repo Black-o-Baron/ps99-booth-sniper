00c6dc9034e7a56635885d0d6eb74e015fe98789
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-02-12 15:07:59 +0530

    Update boothsniper.lua

--- rap-manipulation-shit/boothsniper.lua
+++ rap-manipulation-shit/boothsniper.lua
@@ -27,6 +27,37 @@ local function tryPurchase(uid, playerid, buytimestamp)
     ReplicatedStorage.Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
 end
 
+--=======================================================================================================
+local PURCHASE_PRICE1 = 45678800
+local PURCHASE_PRICE2 = 45592980
+local PURCHASE_PRICE3 = 45764250
+--=======================================================================================================
+local function listHuge1()
+    local args = {
+        [1] = "cb8eda4d6593498e87e9901a5af48163", -- axolotl
+        [2] = PURCHASE_PRICE1,
+        [3] = 1
+    }
+    ReplicatedStorage.Network.Booths_CreateListing:InvokeServer(unpack(args))
+end
+local function listHuge2()
+    local args = {
+        [1] = "e23961d8fb554bfdb6a4419352994eea", -- skeleton
+        [2] = PURCHASE_PRICE2,
+        [3] = 1
+    }
+    ReplicatedStorage.Network.Booths_CreateListing:InvokeServer(unpack(args))
+end
+local function listHuge3()
+    local args = {
+        [1] = "ef4239db53904f0b9ff211c3dd330558", -- mascot
+        [2] = PURCHASE_PRICE3,
+        [3] = 1
+    }
+    ReplicatedStorage.Network.Booths_CreateListing:InvokeServer(unpack(args))
+end
+--=======================================================================================================
+
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
     if type(message) == "table" then
         local highestTimestamp = -math.huge -- Initialize with the smallest possible number
@@ -54,14 +85,20 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
 
             print("ITEM NAME: " .. item)
 
-            if string.find(item, "Huge Hologram Axolotl") and unitGems == 46678800 then
+            if string.find(item, "Huge Hologram Axolotl") and unitGems == PURCHASE_PRICE1 then
                 coroutine.wrap(tryPurchase)(uid, playerid, buytimestamp)
+                task.wait(2)
+                coroutine.wrap(listHuge2)()
                 return
-            elseif string.find(item, "Huge Skeleton") and unitGems == 46678800 then
+            elseif string.find(item, "Huge Skeleton") and unitGems == PURCHASE_PRICE2 then
                 coroutine.wrap(tryPurchase)(uid, playerid, buytimestamp)
+                task.wait(2)
+                coroutine.wrap(listHuge3)()
                 return
-            elseif string.find(item, "Huge BIG Maskot") and unitGems == 46678800 then
+            elseif string.find(item, "Huge BIG Maskot") and unitGems == PURCHASE_PRICE3 then
                 coroutine.wrap(tryPurchase)(uid, playerid, buytimestamp)
+                task.wait(2)
+                coroutine.wrap(listHuge1)()
                 return
             end
         end
